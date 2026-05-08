f008f09343e457e9807cab8c9d6105cd851d0d07
edwinyxxz <edwinncahyo@gmail.com>
2026-05-08 11:52:14 +0700

    Auth with firebase

--- app/src/main/java/id/ac/pnm/e_commercefitin/LoginActivity.kt
+++ app/src/main/java/id/ac/pnm/e_commercefitin/LoginActivity.kt
@@ -10,6 +10,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,23 +23,40 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val editTextEmail: EditText = findViewById()<EditText>(R.id.editTextEmailAddress)
+        val editTextEmail: EditText = findViewById<EditText>(R.id.editTextEmailAddress)
         val editTextPassword: EditText = findViewById<EditText>(R.id.editTextPassword)
         val buttonLogin = findViewById<Button>(R.id.buttonLogin)
         val register = findViewById<TextView>(R.id.textViewRegister)
-        val akunLogin = ProfileFragment.dataTitipan
+//        val akunLogin = ProfileFragment.dataTitipan
+        val database = Firebase.database
+        val users =  database.getReference("users")
+        val usersFromDb = users.child("email")
 
         buttonLogin.setOnClickListener {
             val email: String = editTextEmail.text.toString()
             val password: String = editTextPassword.text.toString()
 
-            if(email == akunLogin?.email && password == akunLogin.password){
-                val intentLogin2Main = Intent(this, MainActivity::class.java)
-                intentLogin2Main.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                startActivity(intentLogin2Main)
-            } else {
-                Toast.makeText(this, "Email atau password salah, silahkan coba lagi", Toast.LENGTH_SHORT).show()
+            usersFromDb.get().addOnSuccessListener {
+                val passwordFromDb = it.child("password").toString()
+
+                if (password==passwordFromDb){
+                    val intentLoginToMain = Intent(this, MainActivity::class.java)
+                    intentLoginToMain.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intentLoginToMain)
+                } else{
+                    Toast.makeText(this, "password salah, silahkan coba lagi", Toast.LENGTH_SHORT).show()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, "email tidak ada", Toast.LENGTH_SHORT).show()
             }
+//
+//            if(email == akunLogin?.email && password == akunLogin.password){
+//                val intentLogin2Main = Intent(this, MainActivity::class.java)
+//                intentLogin2Main.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//                startActivity(intentLogin2Main)
+//            } else {
+//                Toast.makeText(this, "Email atau password salah, silahkan coba lagi", Toast.LENGTH_SHORT).show()
+//            }
         }
         register.setOnClickListener {
             val intentToRegister = Intent(this, RegisterActivity::class.java)
